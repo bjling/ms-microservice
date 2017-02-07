7beb4403b737ed30cb3a29fc4f6b4771b804db54
baochunyu <Aa10051544>
2017-02-07 17:44:59 +0800

    init

--- ms-hello/src/main/java/com/bao/MsHelloApplication.java
+++ ms-hello/src/main/java/com/bao/MsHelloApplication.java
@@ -1,10 +1,36 @@
 package com.bao;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@RestController
+@Slf4j
 @SpringBootApplication
 public class MsHelloApplication {
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@GetMapping("/hello")
+	public String hello() {
+		String world = restTemplate.getForObject("http://localhost:10002/world", String.class);
+		log.info("hello {}", world);
+		return "hello ";
+	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsHelloApplication.class, args);
--- ms-world/src/main/java/com/bao/MsWorldApplication.java
+++ ms-world/src/main/java/com/bao/MsWorldApplication.java
@@ -1,10 +1,21 @@
 package com.bao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RestController
 @SpringBootApplication
 public class MsWorldApplication {
+
+	@GetMapping("/world")
+	public String world(){
+		log.info("world");
+		return "world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsWorldApplication.class, args);
