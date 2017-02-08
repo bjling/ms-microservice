390d4b8a018e313c453cdf73ad82c6ae8502ef83
baochunyu <Aa10051544>
2017-02-08 17:16:54 +0800

    callable

--- ms-hello/src/main/java/com/bao/MsHelloApplication.java
+++ ms-hello/src/main/java/com/bao/MsHelloApplication.java
@@ -5,40 +5,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.Callable;
 
 @RestController
 @Slf4j
 @SpringBootApplication
 public class MsHelloApplication {
 
-	@Bean
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	@Autowired
-	RestTemplate restTemplate;
+    @Autowired
+    RestTemplate restTemplate;
 
-	@GetMapping("/hello")
-	public String hello() {
-		String world = restTemplate.getForObject("http://localhost:10002/world", String.class);
-		log.info("hello {}", world);
-		return "hello ";
-	}
+    @GetMapping("/hello")
+    public String hello() {
+        String world = restTemplate.getForObject("http://localhost:10002/world", String.class);
+        log.info("hello {}", world);
+        return "hello ";
+    }
 
-	@GetMapping("/exception")
-	public String exception() throws Exception {
-		log.info("exception");
-		throw new Exception();
-	}
+    @GetMapping("/exception")
+    public String exception() throws Exception {
+        log.info("exception");
+        throw new Exception();
+    }
+
+    @GetMapping("/callable")
+    public Callable<String> callable() throws InterruptedException {
+        log.info("callable");
+        return () -> {
+            //支付接到第三方通知后可以更新DB，并且放入redis，程序通过查询redis获取支付结果
+            //与前端约定一个时间，用来读取支付结果
+            Thread.sleep(10000);
+            return "callable";
+        };
+    }
 
 
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsHelloApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsHelloApplication.class, args);
+    }
 }
