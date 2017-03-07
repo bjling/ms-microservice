e3ad4b1f6472a7e7080f8628da5158ef0f4dfb7e
baochunyu <Aa10051544>
2017-03-07 17:12:45 +0800

    email

--- ms-hello/src/test/java/com/bao/MsHelloApplicationTests.java
+++ ms-hello/src/test/java/com/bao/MsHelloApplicationTests.java
@@ -49,8 +49,29 @@ public class MsHelloApplicationTests {
 			message.setFrom(new InternetAddress("baochunyu1987@163.com"));
 			message.setRecipients(Message.RecipientType.TO,
 					InternetAddress.parse("baochunyu@ppdai.com"));
-			message.setSubject("apply申请");
-			message.setText("I have a dream ，我要申请");
+			message.setSubject("短信业务申请");
+			message.setText("<html>\n" +
+					"\t<head>\n" +
+					"\t<meta http-equiv='content-type' content='text/html; charset=UTF-8'> </head>\n" +
+					"\t<body>\n" +
+					"\t\thi:\n" +
+					"\t\t<br>\n" +
+					"\t\t&nbsp;&nbsp;&nbsp;&nbsp;部门：科技部\n" +
+					"\t\t<br>\n" +
+					"\t\t&nbsp;&nbsp;&nbsp;&nbsp;姓名：鲍春雨\n" +
+					"\t\t<br>\n" +
+					"\t\t&nbsp;&nbsp;&nbsp;&nbsp;邮箱：baochunyu@ppdai.com\n" +
+					"\t\t<br>\n" +
+					"\t\t&nbsp;&nbsp;&nbsp;&nbsp;申请业务名：催收\n" +
+					"\t\t<br>\n" +
+					"\t\t&nbsp;&nbsp;&nbsp;&nbsp;日预估量：10000\n" +
+					"\t\t<br>\n" +
+					"\t\t&nbsp;&nbsp;&nbsp;&nbsp;模板：这是催收短信{msg}。\n" +
+					"\t\t<br>\n" +
+					"\t\t<br>\n" +
+					"\t\t&nbsp;&nbsp;&nbsp;&nbsp;请予以处理。\n" +
+					"\t</body>\n" +
+					"</html>","UTF-8","html");
 
 			mailSender.send(message);
 		}
