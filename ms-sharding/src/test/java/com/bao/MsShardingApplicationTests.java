c9a22cd7f81dff90dd4db2b730131bdb87ef32cb
baochunyu <baochunyu1987@163.com>
2017-03-12 22:56:27 +0800

    add sharding jdbc

--- ms-sharding/src/test/java/com/bao/MsShardingApplicationTests.java
+++ ms-sharding/src/test/java/com/bao/MsShardingApplicationTests.java
@@ -1,16 +1,33 @@
 package com.bao;
 
+import com.bao.mapper.UserMapper;
+import com.bao.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
 public class MsShardingApplicationTests {
+
+	@Autowired
+	UserMapper userMapper;
 
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void insertTetst(){
+		User user = User.builder().name("bao").userId(1).age(30).build();
+		int i = userMapper.insertSelective(user);
+		assertEquals(1,i);
+	}
+
 
 }
