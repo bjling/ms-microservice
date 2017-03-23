855b028fd13cc8dfbb9ee8320373b3e304e063f3
baochunyu <baochunyu@baochunyudeMacBook-Pro.local>
2017-03-23 15:43:14 +0800

    ms-netty

---
+++ ms-spring-batch/src/main/java/com/bao/MsSpringBatchApplication.java
@@ -0,0 +1,12 @@
+package com.bao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringBatchApplication.class, args);
+	}
+}
